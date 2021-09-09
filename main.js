acaa5cd3dce04b6e306cddeb0f2b58a169c6319c
Michael <Mlcorrieri@gmail.com>
2021-09-09 14:47:04 -0400

    stars added

--- main.js
+++ main.js
@@ -39,8 +39,23 @@ scene.add(pointLight, ambLight);
 const lightHelp = new THREE.PointLightHelper(pointLight);
 const gridHelp = new THREE.GridHelper(200, 50);
 scene.add(lightHelp, gridHelp);
-
+// pointer control
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 function animate() {
   requestAnimationFrame(animate);
