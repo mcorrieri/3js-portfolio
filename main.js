61b261a7ff141df2b4907d076ed43e98df3560ba
Michael <Mlcorrieri@gmail.com>
2021-09-09 14:35:51 -0400

    lights added, light and gridhelp added

--- main.js
+++ main.js
@@ -21,13 +21,23 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  wireframe: true,
+const material = new THREE.MeshStandardMaterial({
+  color: "orange",
+  //   wireframe: true,
 });
 const torusKnot = new THREE.Mesh(geometry, material);
 
 scene.add(torusKnot);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambLight);
+
+// wireframe pointers
+const lightHelp = new THREE.PointLightHelper(pointLight);
+const gridHelp = new THREE.GridHelper(200, 50);
+scene.add(lightHelp, gridHelp);
 
 function animate() {
   requestAnimationFrame(animate);
